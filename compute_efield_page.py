b21403f2cb40bc50b6a0593290267628171cebf2
John Feng <john.feng@redlen.com>
2025-05-24 18:07:29 -0700

    removed the st.image commands since it causes error

--- compute_efield_page.py
+++ compute_efield_page.py
@@ -145,10 +145,6 @@ elif data_source == "Sample Data":
 with st.expander("Check Data"):
     st.write(f"Number of calibration files: {len(uploaded_calib_files)}")
     st.write(f"Number of bias files: {len(uploaded_data_files)}")
-    for uploaded_calib_file in uploaded_calib_files:
-        st.image(uploaded_calib_file)
-    for uploaded_data_file in uploaded_data_files:
-        st.image(uploaded_data_file)
     
 calib_img_arrays = {}
 
